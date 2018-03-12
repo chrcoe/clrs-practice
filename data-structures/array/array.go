ce547d62e4bcc8474f7d8db9686bd725e03738e8
Chris Coe <chrcoe01@gmail.com>
2018-03-12 14:56:25 -0400

    finished array

--- data-structures/array/array.go
+++ data-structures/array/array.go
@@ -3,7 +3,8 @@ package array
 import "fmt"
 
 // Array implements an array attempting to not use any built in arrays.  Kind of silly, but it is a
-// starting point for the rest of the code-kata.
+// starting point for the rest of the code-kata.  Unfortunately I cannot seem to find any other way
+// to create a manual array by allocating memory blocks etc ..
 type Array struct {
 	array []interface{}
 	len   int // how large do we want to make this ?
@@ -11,15 +12,15 @@ type Array struct {
 
 // New provides a new array.
 func New(elemType interface{}, len int) *Array {
-	// https://stackoverflow.com/questions/30716354/how-do-i-do-a-literal-int64-in-go
-	//
-	fmt.Println(elemType)
 	t := make([]interface{}, len)
 	return &Array{t, len}
 }
 
 // Traverse will print out all of the elements in the array one by one.
 func (a *Array) Traverse() {
+	for i, e := range a.array {
+		fmt.Printf("index: %d element: %d\n", i, e)
+	}
 }
 
 // Insert adds an element at the given index.
@@ -30,6 +31,7 @@ func (a *Array) Insert(value interface{}, index int) {
 // Delete removes an element at the given index if the index is within the length of the array,
 // otherwise it will throw an exception.
 func (a *Array) Delete(index int) {
+	a.array[index] = nil
 }
 
 // SearchByIndex returns the value of the element at the given index if it exists, or nil if it does
@@ -39,12 +41,18 @@ func (a *Array) SearchByIndex(index int) interface{} {
 }
 
 // SearchByValue returns the index of the value if it exists, or nil if it does not.
-func (a *Array) SearchByValue(value interface{}) interface{} {
-	return nil
+func (a *Array) SearchByValue(value interface{}) int {
+	for i, e := range a.array {
+		if e == value {
+			return i
+		}
+	}
+	return -1
 }
 
 // Update will update an element at the given index.
-func (a *Array) Update(index int) {
+func (a *Array) Update(value interface{}, index int) {
+	a.array[index] = value
 }
 
 // Len returns the length of the array.
--- data-structures/array/array_test.go
+++ data-structures/array/array_test.go
@@ -13,8 +13,67 @@ func TestNew(t *testing.T) {
 		t.Error()
 	}
 
+	if a.array == nil {
+		t.Error()
+	}
+
+	// how to test traversal - this isn't providing an iterator directly ..
+	a.Traverse()
+
+}
+
+func TestInsert(t *testing.T) {
+	a := New(types.String, 10)
 	a.Insert("test", 2)
 	if a.SearchByIndex(2) != "test" {
 		t.Error()
 	}
+}
+
+func TestDelete(t *testing.T) {
+	a := New(types.String, 10)
+	a.Insert("test", 2)
+	a.Delete(2)
+	if a.SearchByIndex(2) != nil {
+		t.Error()
+	}
+}
+
+func TestSearchByIndex(t *testing.T) {
+	a := New(types.String, 10)
+	for i := 0; i < a.Len(); i++ {
+		if a.SearchByIndex(i) != nil {
+			t.Error()
+		}
+	}
+
+	a.Insert("test", 2)
+	if a.SearchByIndex(2) != "test" {
+		t.Error()
+	}
+}
+
+func TestSearchByValue(t *testing.T) {
+	a := New(types.String, 10)
+	if a.SearchByValue("test") != -1 {
+		t.Error()
+	}
+
+	a.Insert("test", 3)
+	if a.SearchByValue("test") != 3 {
+		t.Error()
+	}
+}
+
+func TestUpdate(t *testing.T) {
+	a := New(types.String, 10)
+	if a.SearchByIndex(2) != nil {
+		t.Error()
+	}
+
+	a.Update("test", 2)
+	if a.SearchByIndex(2) != "test" {
+		t.Error()
+	}
+
 }
